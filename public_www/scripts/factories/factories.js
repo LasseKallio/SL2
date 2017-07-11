26e160430c3a4b6a00c04a9bfb2a592866ebf04a
Lasse Kallio <lasse11@dlc.fi>
2017-07-11 14:52:18 +0300

    empty factorories

--- public_www/scripts/factories/factories.js
+++ public_www/scripts/factories/factories.js
@@ -1,22 +1,36 @@
 var app = angular.module("Factories",["ngResource"]);
 
 app.factory("ShoppingItem", function($resource, TokenFactory) {
+	
+return $resource('/api/shoppinglist/:id', 
+                        {id: '@id'}, 
+                        {'query': {method: 'GET'}});
 
-		});
+
+
+});
 	
 
 
+
 app.factory("TokenFactory", function() {
+
+	var TokenFactory = {};
+	return {};
 
 	
 });
 
 app.factory("LoginFactory", function($http) {
+
+		var LoginFactory = {};
+	return {};
 	
 
 		
 }); 
 app.factory("UiFactory", function() {
 
-	
+	var UiFactory = {};
+	return {};
 });
